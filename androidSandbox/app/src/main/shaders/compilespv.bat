3a05d6a9202314a99e0aee682c524b18ea6ac8dc
xiaojunwa <wxj995@gmail.com>
2023-09-27 09:51:28 -0700

    restructure compute kernel

--- androidSandbox/app/src/main/shaders/compilespv.bat
+++ androidSandbox/app/src/main/shaders/compilespv.bat
@@ -4,5 +4,6 @@ for /d /r %%i in (*) do (
     if exist %%i/shader.frag (%VULKAN_SDK%/Bin/glslc.exe %%i/shader.frag -o %%i/frag.spv)
     if exist %%i/shader.comp (%VULKAN_SDK%/Bin/glslc.exe %%i/shader.comp -o %%i/comp.spv)
     if exist %%i/shaderTranspose.comp (%VULKAN_SDK%/Bin/glslc.exe %%i/shaderTranspose.comp -o %%i/compTranspose.spv)
+    if exist %%i/shaderOptimizeMatMul.comp (%VULKAN_SDK%/Bin/glslc.exe %%i/shaderOptimizeMatMul.comp -o %%i/compOptimizeMatMul.spv)
 )
 
