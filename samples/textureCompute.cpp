28caa93b154a8b98e1160209e52e07c7cd50242f
xiaojunwa <wxj995@gmail.com>
2023-11-17 16:10:00 -0800

    add texture_eye.png, update textureCompute sample

--- samples/textureCompute.cpp
+++ samples/textureCompute.cpp
@@ -4,20 +4,20 @@
 class TEST_CLASS_NAME: public CApplication{
 public:
 	//Compute Shader Constants
-	static const int DIM = 16;
-	static const int DIM_M = DIM;
-	static const int DIM_K = DIM;
-	static const int DIM_N = DIM;
+	// static const int DIM = 16;
+	// static const int DIM_M = DIM;
+	// static const int DIM_K = DIM;
+	// static const int DIM_N = DIM;
 	static const int KernelRunNumber = 1;
-	struct StructStorageBuffer {
-		unsigned int M;
-		unsigned int K;
-		unsigned int N;
-		float MatA[DIM_M * DIM_K];
-		float MatB[DIM_K * DIM_N];
-		float MatC[DIM_M * DIM_N];
-	};
-	StructStorageBuffer storageBufferObject;
+	// struct StructStorageBuffer {
+	// 	unsigned int M;
+	// 	unsigned int K;
+	// 	unsigned int N;
+	// 	float MatA[DIM_M * DIM_K];
+	// 	float MatB[DIM_K * DIM_N];
+	// 	float MatC[DIM_M * DIM_N];
+	// };
+	// StructStorageBuffer storageBufferObject;
 	// struct StructStorageBufferOutput {
 	// 	float MatC[DIM_M * DIM_N];
 	// };
@@ -44,8 +44,8 @@ public:
 		
 		//For Graphics
 		renderer.CreateGraphicsCommandBuffer(); 
-		VkImageUsageFlags usage = VK_IMAGE_USAGE_TRANSFER_DST_BIT | VK_IMAGE_USAGE_SAMPLED_BIT;
-		textureImages[0].CreateTextureImage("texture.jpg", usage, renderer.commandPool);
+		VkImageUsageFlags usage = VK_IMAGE_USAGE_TRANSFER_DST_BIT | VK_IMAGE_USAGE_SAMPLED_BIT | VK_IMAGE_USAGE_STORAGE_BIT;
+		textureImages[0].CreateTextureImage("texture_eye.png", usage, renderer.commandPool);
 		textureImages[0].CreateImageView(VK_IMAGE_ASPECT_COLOR_BIT);
 
 		//For Compute
@@ -78,17 +78,24 @@ public:
 		std::cout<<"graphcis descriptor created."<<std::endl;
 
 		//For Compute
-		descriptors[1].addStorageBuffer(sizeof(StructStorageBuffer));
+		//descriptors[1].addStorageBuffer(sizeof(StructStorageBuffer));
 		//std::cout<<"storageBufferObjectInput added. Size = "<<DIM_M * DIM_K * 8.0f * 3 / 1024 / 1024<<"mb."<<std::endl;
-		//descriptors[0].addStorageBuffer(sizeof(StructStorageBufferOutput));
+		//descriptors[1].addStorageBuffer(sizeof(StructStorageBufferOutput));
 		//std::cout<<"storageBufferObjectOutput added. Size = "<<DIM_M * DIM_K * 8.0f / 1024 / 1024<<"mb."<<std::endl;
+		descriptors[1].addStorageImage(textureImages[0].textureImageBuffer.size);//assume 4 channels and 4 bytes/channel
+		//std::cout<<"storageImage added. Size = "<<textureImages[0].texHeight * textureImages[0].texWidth * 4 * 4<<" bytes."<<std::endl;
+		//descriptors[1].addStorageImage(sizeof(StructStorageBuffer));
+		std::cout<<"addStorageImage() done."<<std::endl;
 		descriptors[1].createDescriptorPool();
+		std::cout<<"createDescriptorPool() done."<<std::endl;
 		descriptors[1].createDescriptorSetLayout();
-		descriptors[1].createDescriptorSets();
+		std::cout<<"createDescriptorSetLayout() done."<<std::endl;
+		descriptors[1].createDescriptorSets(&textureImages);
 		std::cout<<"compute descriptor created."<<std::endl;
 
 		//for Graphics: when create graphics pipeline, use descriptor set 0 layout
 		renderProcess.createGraphicsPipelineLayout(descriptors[0].descriptorSetLayout);
+		std::cout<<"createGraphicsPipelineLayout() done."<<std::endl;
 		renderProcess.createGraphicsPipeline<Vertex3D>(
 			VK_PRIMITIVE_TOPOLOGY_TRIANGLE_LIST, 
 			shaderManager.vertShaderModule, 
@@ -102,27 +109,27 @@ public:
 		CApplication::initialize();
 
 		//Initial Host data
-		storageBufferObject.M = DIM_M;
-		storageBufferObject.N = DIM_N;
-		storageBufferObject.K = DIM_K;
-		for(int i = 0; i < DIM_M*DIM_K; i++) storageBufferObject.MatA[i] = (float)rand() / (float)RAND_MAX;
-		for(int i = 0; i < DIM_K*DIM_N; i++) storageBufferObject.MatB[i] = (float)rand() / (float)RAND_MAX;
-		//if(bVerbose) printMatrix(storageBufferObjectInput.MatA, DIM_M, DIM_K, "A");
-		//if(bVerbose) printMatrix(storageBufferObjectInput.MatB, DIM_K, DIM_N, "B");
-		if(bVerbose) PRINT("A: ", storageBufferObject.MatA, DIM_M*DIM_K);
-		if(bVerbose) PRINT("B: ", storageBufferObject.MatB, DIM_K*DIM_N);
-		if(bVerbose) PRINT("");
-		std::cout<<"Initialized A and B."<<std::endl;
+		// storageBufferObject.M = DIM_M;
+		// storageBufferObject.N = DIM_N;
+		// storageBufferObject.K = DIM_K;
+		// for(int i = 0; i < DIM_M*DIM_K; i++) storageBufferObject.MatA[i] = (float)rand() / (float)RAND_MAX;
+		// for(int i = 0; i < DIM_K*DIM_N; i++) storageBufferObject.MatB[i] = (float)rand() / (float)RAND_MAX;
+		// //if(bVerbose) printMatrix(storageBufferObjectInput.MatA, DIM_M, DIM_K, "A");
+		// //if(bVerbose) printMatrix(storageBufferObjectInput.MatB, DIM_K, DIM_N, "B");
+		// if(bVerbose) PRINT("A: ", storageBufferObject.MatA, DIM_M*DIM_K);
+		// if(bVerbose) PRINT("B: ", storageBufferObject.MatB, DIM_K*DIM_N);
+		// if(bVerbose) PRINT("");
+		// std::cout<<"Initialized A and B."<<std::endl;
 
 		//Host >> Device
-		descriptors[1].updateStorageBuffer<StructStorageBuffer>(renderer.currentFrame, durationTime, storageBufferObject); //1 TODO: fill all inflight, update input only
-		descriptors[1].updateStorageBuffer<StructStorageBuffer>(renderer.currentFrame+1, durationTime, storageBufferObject); //1
+		//descriptors[1].updateStorageBuffer<StructStorageBuffer>(renderer.currentFrame, durationTime, storageBufferObject); //1 TODO: fill all inflight, update input only
+		//descriptors[1].updateStorageBuffer<StructStorageBuffer>(renderer.currentFrame+1, durationTime, storageBufferObject); //1
 		
 	}
 
 	void update(){
 		static int counter = 1;
-		if(counter==KernelRunNumber) NeedToExit = false;
+		if(counter==KernelRunNumber) NeedToExit = true;
 		counter++;
 		
 		CApplication::update(); //update deltaTime and durationTime (and mainCamera and MVP, VP)
@@ -149,8 +156,8 @@ public:
 		START_COMPUTE_RECORD(1)
 
 		//std::cout<<"Record Compute command buffer. "<<std::endl;
-		renderer.Dispatch(1, 1, 1);
-		std::cout<<"Dispatched."<<std::endl;
+		renderer.Dispatch(100, 1, 1);
+		//std::cout<<"Dispatched."<<std::endl;
 
 		END_COMPUTE_RECORD
 	}
@@ -164,67 +171,36 @@ public:
 
 		//Device >> Host
 		//if(bVerbose) memcpy(storageBufferObject.MatC, descriptors[0].storageBuffersMapped[renderer.currentFrame], sizeof(storageBufferObject.MatC));//2
-		if(bVerbose) memcpy(&storageBufferObject, descriptors[1].storageBuffersMapped[renderer.currentFrame], sizeof(storageBufferObject));//2 TODO: how to copy only MatC
+		//if(bVerbose) memcpy(&storageBufferObject, descriptors[1].storageBuffersMapped[renderer.currentFrame], sizeof(storageBufferObject));//2 TODO: how to copy only MatC
 		//if(bVerbose) printMatrix(storageBufferObjectOutput.MatC, DIM_M, DIM_N, "C");
-		if(bVerbose) PRINT("C: ", storageBufferObject.MatC, DIM_M*DIM_N);
+		//if(bVerbose) PRINT("C: ", storageBufferObject.MatC, DIM_M*DIM_N);
 
-		if(bVerify){
-			//std::cout<<"Begin verification..."<<std::endl;
-			PRINT("Begin verification... ");
-			float cpu_result[DIM_M*DIM_N];
-			CPUSingleThreadMatMul(DIM_M, DIM_N, DIM_K, storageBufferObject.MatA, storageBufferObject.MatB, cpu_result, DIM_M*DIM_N);
-			//printMatrix(cpu_result, DIM_M, DIM_N, "cpu_C");
-			PRINT("cpu_C: ", cpu_result, DIM_M*DIM_N);
-		}
 
-		if(bVerbose) PRINT("");
+	    std::cout<<textureImages[0].textureImageBuffer.size<<std::endl;
+		//memcpy(textureImages[0].textureImageBuffer.deviceMemory, descriptors[1].storageImagesMapped[renderer.currentFrame], textureImages[0].textureImageBuffer.size);
+		
+		float data[textureImages[0].textureImageBuffer.size];
+		memcpy(data, descriptors[1].storageImagesMapped[renderer.currentFrame], textureImages[0].textureImageBuffer.size);
+		for(int i = 0; i < textureImages[0].textureImageBuffer.size; i++)
+			PRINT("%f",data[i]);
+
+
+		std::cout<<"done"<<std::endl;
+
+		// if(bVerify){
+		// 	//std::cout<<"Begin verification..."<<std::endl;
+		// 	PRINT("Begin verification... ");
+		// 	float cpu_result[DIM_M*DIM_N];
+		// 	CPUSingleThreadMatMul(DIM_M, DIM_N, DIM_K, storageBufferObject.MatA, storageBufferObject.MatB, cpu_result, DIM_M*DIM_N);
+		// 	//printMatrix(cpu_result, DIM_M, DIM_N, "cpu_C");
+		// 	PRINT("cpu_C: ", cpu_result, DIM_M*DIM_N);
+		// }
+
+		//if(bVerbose) PRINT("");
 	}
 
-/*
-	void printMatrix(float *matA, int DIM_M, int DIM_N, std::string matName){
-#ifndef ANDROID		
-		std::cout<<"Print "<<matName<<std::endl;
-		for(int i=0; i < DIM_M; i++){
-			for(int j=0; j < DIM_N; j++){
-				std::cout<<matA[i*DIM_N + j]<<", ";
-			}
-			std::cout<<std::endl;
-		}
-		std::cout<<std::endl;
-#else
-		__android_log_print(ANDROID_LOG_VERBOSE, LOG_TAG, "Print %s\n", matName.c_str());
-		for(int i=0; i < DIM_M; i++){
-			for(int j=0; j < DIM_N; j++){
-				__android_log_print(ANDROID_LOG_VERBOSE, LOG_TAG, "%f, ", matA[i*DIM_N + j]);
-			}
-			//__android_log_print(ANDROID_LOG_VERBOSE, LOG_TAG, "\n");
-		}
-		__android_log_print(ANDROID_LOG_VERBOSE, LOG_TAG, "\n");
-#endif
-	}
-*/
-	void CPUSingleThreadMatMul(int M, int N, int K, float *matrixA, float *matrixB, float *outputMatrix, int sampleNum){
-		int count = 0;
-		int printDelta = sampleNum / 1;
-		for(int n = 0; n < N; n++){ //col
-			for(int m = 0; m < M; m++){ //row： fill row first, allign with kernel
-				//column major matrix multiplication
-				outputMatrix[n*M + m] = 0;
-				for(int k = 0; k < K; k++){
-					outputMatrix[n*M + m] += matrixA[k*M + m] * matrixB[n*K + k];
-				}
 
-				count++;
-				if(count % printDelta == 0){
-					float completeRate = (count * 100.0)/sampleNum ;
-					PRINT("Completed: %f%%", completeRate);
-					//std::cout<<"Completed: "<<completeRate<<"%"<<std::endl;
-				}
-				
-				if(count >= sampleNum) return;
-			}
-		}
-	}
+
 };
 
 #ifndef ANDROID
