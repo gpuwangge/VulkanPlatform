9bdeaf771240129fe971a2cdce12e91bd6726744
Connor <wxj995@gmail.com>
2024-11-03 01:22:14 -0700

    add simpleCubemap.cpp

---
+++ samples/simpleCubemap.cpp
@@ -0,0 +1,34 @@
+#include "..\\vulkanFramework\\include\\application.h"
+#define TEST_CLASS_NAME CSimpleCubemap
+class TEST_CLASS_NAME: public CApplication{
+public:
+    void initialize(){
+		//mainCamera.setPosition(glm::vec3(0.0f, -2.5f, -2.5f));
+		//mainCamera.setRotation(glm::vec3(45.0f, 0.0f, 0.0f));
+		//mainCamera.setPerspective(60.0f, (float)WINDOW_WIDTH / (float)WINDOW_HEIGHT, 0.1f, 256.0f);
+		appInfo.Object.Count = 1;
+		appInfo.Object.Model.Names = std::make_unique<std::vector<std::string>>(std::vector<std::string> {"viking_room.obj"});
+		appInfo.Object.Model.List = std::make_unique<std::vector<int>>(std::vector<int> {0});
+		appInfo.Object.Texture.Names = std::make_unique<std::vector<std::pair<std::string, bool>>>(std::vector<std::pair<std::string, bool>> {{"viking_room.png", false}});
+		appInfo.Object.Texture.List = std::make_unique<std::vector<int>>(std::vector<int> {0});
+		appInfo.Shader.Vertex = "simpleCubemap/vert.spv";
+		appInfo.Shader.Fragment = "simpleCubemap/frag.spv";	
+		appInfo.Uniform.GraphicsSampler.Count = 1;
+		appInfo.Uniform.EnableGraphicsMVP = true;
+		appInfo.Feature.EnableGraphicsMSAA = true;
+		CApplication::initialize();
+	}
+
+	void update(){
+		objectList[0].SetAngularVelocity(50,50,50);
+		CApplication::update();
+	}
+
+	void recordGraphicsCommandBuffer(){
+		objectList[0].Draw();
+	}
+};
+
+#ifndef ANDROID
+#include "..\\vulkanFramework\\include\\main.hpp"
+#endif
