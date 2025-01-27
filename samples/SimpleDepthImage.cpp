17d28f2c10a098e83d860fe49b335c759b4125fd
Connor <wxj995@gmail.com>
2025-01-26 23:33:31 -0800

    add new sampler: simpleDepthImage

---
+++ samples/SimpleDepthImage.cpp
@@ -0,0 +1,74 @@
+#include "..\\vulkanFramework\\include\\application.h"
+#define TEST_CLASS_NAME CSimpleDepthImage
+class TEST_CLASS_NAME: public CApplication{
+public:
+	struct CustomUniformBufferObject {
+		glm::vec3 lightPos;
+		glm::mat4 lightSpace;
+
+		static VkDescriptorSetLayoutBinding GetBinding(){
+			VkDescriptorSetLayoutBinding binding;
+			binding.binding = 0;//not important, will be reset
+			binding.descriptorCount = 1;
+			binding.descriptorType = VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER;
+			binding.pImmutableSamplers = nullptr;
+			binding.stageFlags = VK_SHADER_STAGE_VERTEX_BIT;
+			return binding;
+		}
+	};
+	CustomUniformBufferObject customUBO{};
+
+	void initialize(){
+		appInfo.Uniform.GraphicsCustom.Size = sizeof(CustomUniformBufferObject);
+		appInfo.Uniform.GraphicsCustom.Binding = CustomUniformBufferObject::GetBinding();
+		CApplication::initialize();
+
+		//objectList[0].SetRotation(-135,0,45);
+		//objectList[0].YawLeft(90,200);
+		//objectList[0].RollLeft(90, 200);
+	} 
+
+	void update(){
+		// float near_plane = 1.0f, far_plane = 7.5f;
+		// glm::mat4 lightProjection = glm::ortho(-10.0f, 10.0f, -10.0f, 10.0f, near_plane, far_plane); 
+		// glm::mat4 lightView = glm::lookAt(
+		// 	glm::vec3(-2.0f, 4.0f, -1.0f), 
+		// 	glm::vec3( 0.0f, 0.0f,  0.0f),  
+		// 	glm::vec3( 0.0f, 1.0f,  0.0f));  
+ 
+		// customUBO.lightPos = glm::vec3(0.75f * sin(durationTime * 3), 0.75f * sin(durationTime * 2), 0.75f * sin(durationTime * 1));
+		// float lightFOV = 10.0f; //45.0f;
+		// float zNear = 0.0f; //1.0f
+		// float zFar = 5.0f; //96.0f
+		// glm::mat4 depthProjectionMatrix = glm::perspective(glm::radians(lightFOV), 1.0f, zNear, zFar); // Matrix from light's point of view
+		// glm::mat4 depthViewMatrix = glm::lookAt(customUBO.lightPos, glm::vec3(0.0f), glm::vec3(0, 1, 0));
+		// glm::mat4 depthModelMatrix = glm::mat4(1.0f);
+		// customUBO.lightSpace = depthProjectionMatrix * depthViewMatrix * depthModelMatrix;
+		
+		// graphicsDescriptorManager.updateCustomUniformBuffer<CustomUniformBufferObject>(renderer.currentFrame, durationTime, customUBO);
+
+
+
+		for(int i = 0; i < lights.size(); i++) {
+			lights[i].SetLightPosition(
+				glm::vec3(0, lights[i].GetLightPosition().y,0) +
+				glm::vec3(2.5 *cos(durationTime * (i+1)), 0, 2.5 *sin(durationTime * (i+1)))
+			);
+			objects[2+i].SetPosition(lights[i].GetLightPosition()); 
+		}
+
+
+		CApplication::update();
+	}
+
+	void recordGraphicsCommandBuffer(){
+		for(int i = 0; i < objects.size(); i++) objects[i].Draw();
+		//vkCmdNextSubpass(renderer.commandBuffers[renderer.graphicsCmdId][renderer.currentFrame], VK_SUBPASS_CONTENTS_INLINE);
+		//for(int i = 0; i < objects.size(); i++) objects[i].Draw();
+		//NeedToExit = true;
+	}
+};
+
+#ifndef ANDROID
+#include "..\\vulkanFramework\\include\\main.hpp"
+#endif
